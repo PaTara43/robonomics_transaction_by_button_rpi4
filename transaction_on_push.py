b9ff348beacdd8cdf10855289edeb15a0a05538f
PaTara43 <p040399@outlook.com>
2020-11-10 19:12:32 +0300

    testing config and callback

---
+++ transaction_on_push.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import logging
+import os
+import time
+import yaml
+
+def button_callback(channel, config):
+    print(config['transaction']['path_to_robonomics_file'])
+
+def read_configuration(dirname) -> dict:
+
+    config_path = dirname + '/config.yaml'
+    logging.debug(config_path)
+
+    try:
+        with open(config_path) as f:
+            content = f.read()
+            config = yaml.load(content, Loader=yaml.FullLoader)
+            logging.debug(f"Configuration dict: {content}")
+            return config
+    except Exception as e:
+        while True:
+            logging.error("Error in configuration file!")
+            logging.error(e)
+            exit()
+
+class Error(Exception):
+    pass
+
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+config = read_configuration(dirname)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(18,GPIO.RISING,callback=button_callback(config), bouncetime=2000) # Setup event on pin 12 rising edge
+
+message = input("Waiting fo button to be pressed\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
