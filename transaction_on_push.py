41af1c968da1a1e642ca53ef0f805897ab24f095
PaTara43 <p040399@outlook.com>
2021-02-15 13:44:13 +0300

    catch impulses

--- transaction_on_push.py
+++ transaction_on_push.py
@@ -5,24 +5,38 @@ import subprocess
 import time
 import yaml
 
+from threading import Thread
 
 global pressed
 
+def idling():
+    while True:
+        time.sleep(60)
+
 def button_callback(channel):
+    #logging.warning("Button pressed")
+    time.sleep(0.1)
+    if GPIO.input(channel):
+        return False
+    #logging.warning("long press")
     global pressed
     if pressed == True:
         program = "echo \"OFF\" | " + config['transaction']['path_to_robonomics_file'] + \
-            " io write launch " + config['transaction']['remote'] + " -s " + config['transaction']['key'] + " -r " + config['transaction']['address']
+            " io write launch " + config['transaction']['remote'] + " -s " + config['transaction']['key']
+ + " -r " + config['transaction']['address']
         process = subprocess.Popen(program, shell=True, stdout=subprocess.PIPE)
         output = process.stdout.readline()
         logging.warning("OFF transaction hash is " + output.strip().decode('utf8'))
+        #logging.warning('OFF')
         pressed = False
+
     else:
         program = "echo \"ON\" | " + config['transaction']['path_to_robonomics_file'] + \
             " io write launch " + config['transaction']['remote'] + " -s " + config['transaction']['key'] + " -r " + config['transaction']['address']
         process = subprocess.Popen(program, shell=True, stdout=subprocess.PIPE)
         output = process.stdout.readline()
         logging.warning("ON transaction hash is " + output.strip().decode('utf8'))
+        #logging.warning('ON')
         pressed = True
 
 def read_configuration(dirname) -> dict:
@@ -53,7 +67,10 @@ pressed = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(18,GPIO.RISING,callback=button_callback, bouncetime=2000) # Setup event on pin 12 rising edge
+GPIO.add_event_detect(18,GPIO.RISING,callback=button_callback, bouncetime=1000) # Setup event on pin 12 rising edge
 
-message = input("Waiting fo button to be pressed\n\n") # Run until someone presses enter
+input("Waiting for button to be pressed") # Run until someone presses enter
 GPIO.cleanup() # Clean up
+
+#start_idling_thread = Thread(target=idling, args=())
+#start_idling_thread.start()
